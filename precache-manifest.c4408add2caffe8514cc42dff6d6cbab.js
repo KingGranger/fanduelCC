1e12ee05dcf45fe15a38e31c755585a13462dea2
KingGranger <cheeridon@gmail.com>
2019-08-13 16:24:32 -0400

    Updates

---
+++ precache-manifest.c4408add2caffe8514cc42dff6d6cbab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf53d3bcf2594a0f0a4098264ae02903",
+    "url": "/fanduelCC/index.html"
+  },
+  {
+    "revision": "f167075d749cb9915083",
+    "url": "/fanduelCC/static/css/main.e00c0e49.chunk.css"
+  },
+  {
+    "revision": "1037788f81fbcddab474",
+    "url": "/fanduelCC/static/js/2.84eb6c65.chunk.js"
+  },
+  {
+    "revision": "f167075d749cb9915083",
+    "url": "/fanduelCC/static/js/main.8fc1d746.chunk.js"
+  },
+  {
+    "revision": "47a3dceddf57fd97ed67",
+    "url": "/fanduelCC/static/js/runtime~main.caf35000.js"
+  }
+]);
